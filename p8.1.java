065dd8c7179826473efe9cf78e8e88bad093b6f8
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-05-06 18:47:12 -0500

    Update p8.1.java

--- p8.1.java
+++ p8.1.java
@@ -1,1 +1,14 @@
+//  Mark Villa
+//  4/20/18
+//  Fibonacci
 
+public class P8.1 {
+  
+  public static void main(Strings arg[]) {
+    System.out.println(fib(5));
+  }
+  
+  public static int fib(int n) {
+	    return (n <= 1) ? n: fib(n-1) + fib(n-2);
+  }
+}
